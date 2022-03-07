5e4d895a0ac42b5cd35e5c528284169013f12610
“username” <angeljunotruffles@gmail.com>
2022-03-07 00:26:37 -0500

    jSBasics

---
+++ script.js
@@ -0,0 +1,62 @@
+//1
+console.log(27);
+//2
+/*
+Strings
+Numbers
+Booleans
+Undefined
+Null
+*/
+//3
+/*
+Addition
+Subtraction
+Multiplication
+Division
+Exponentiation
+Modulus
+*/
+//4
+//Modulus retains the remainder in division problems. 
+console.log(5%4);
+console.log(5%2);
+//5
+console.log('I\'\M MARY POPPINS, Y\'\ALL!');
+//6
+let x = 'Hawk'
+let y = 'eye'
+console.log(x + y);
+//7
+x = 'g'
+y = '.g.'
+console.log((x+y).length);
+//8
+x = 'Avengers: Endgame'
+console.log(x[16]);
+//BONUS
+console.log('\\');
+
+console.log( 5 ** 4 - 9);
+
+x = 'I'
+y = 'Am'
+let z = 'Iron'
+let n = 'Man'
+console.log(x.length - y.length + (z.length - n.length));
+
+x = 'Cap'
+y = 'Vision'
+z = 'Oyoke'
+n = 'Nick Fury'
+let m = 'Groot'
+let o = 'Doctor Strange'
+let p = 'Thor'
+let q = 'Star Lord'
+let r = x[1] + y[0] + z[4] + n[0] + m[0]+ o[13] + p[3] + q[0]
+console.log(r.toUpperCase());
+
+m = 25
+let c = 4
+let u = m + c
+console.log(u);
